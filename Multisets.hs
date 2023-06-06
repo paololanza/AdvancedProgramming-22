3a539324423c205ef1322eef9f9244947cc0a7cc
paololanza <paolo.lanza.97@gmail.com>
2023-06-06 10:02:17 +0200

    initial haskell implementation

--- Multisets.hs
+++ Multisets.hs
@@ -1,20 +1,20 @@
+module Ex1(
+    MSet(MS),
+    empty,
+    add,
+    findElement
+) where
+
 data MSet a = MS [(a, Int)]
     deriving (Show)
 
-findElement :: Eq a => a -> ListBag a -> Bool
-findElement a (LB []) = False
-findElement a (LB ((x,y):xs)) = x == a || findElement a (LB xs)
+findElement :: Eq a => a -> MSet a -> Bool
+findElement a (MS []) = False
+findElement a (MS ((x,y):xs)) = x == a || findElement a (MS xs)
 
 empty :: MSet a
 empty = MS []
 
-add :: 
-findElement 
-
-occs ::
-
-elems ::
-
-subeq ::
-
-union ::
+add :: Eq a => MSet a -> a -> MSet a
+add (MS[]) v = MS([(v, 1)])
+add (MS((x, y):xs)) v = if x==v then (MS ((x,y+1):xs)) else add (MS xs) v
